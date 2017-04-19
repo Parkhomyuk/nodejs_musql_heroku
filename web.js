d01e29eaa0cf78506b281f4d4344be03782b3037
Alexey Parkhomyuk <parkhomyukalex@gmail.com>
2017-04-19 11:28:56 +0300

    commit 1

--- web.js
+++ web.js
@@ -1,12 +1,11 @@
+var cool = require('cool-ascii-faces');
 var express = require('express');
 var app = express();
-app.use(express.logger());
 
-app.get('/', function(req, res){
-    res.send('Hi');
+app.set('port', (process.env.PORT || 5000));
+app.get('/', function(request, response) {
+    response.send('aweryone hello!');
 });
-
-var port = process.env.PORT || 5000;
-app.listen(port,function(){
-    console.log("Listing on port :"+port);
-});
+app.listen(app.get('port'), function() {
+    console.log('Node app is running on port', app.get('port'));
+})
